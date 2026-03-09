185a58d8dbccc7b5e9aa8db2cdf9ade6c2833bc0
GabrielaSzuiogan <Gabriela.Szuiogan@cognizant.com>
2026-03-09 16:13:57 +0200

    added sqlalchemy

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/")
+def read_root():
+    return {"Post": "Message"}
+
+@app.put("/")
+def read_root():
+    return {"Put": "Message"}
+
+@app.delete("/")
+def read_root():
+    return {"Delete": "Message"}
+
+
 def main():
     uvicorn.run("main:app", port=8000, host="0.0.0.0", reload= True)
 
