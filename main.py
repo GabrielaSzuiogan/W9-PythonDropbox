8f58aa86936e353e6389fbfd9fb99db7bed2a813
GabrielaSzuiogan <Gabriela.Szuiogan@cognizant.com>
2026-03-09 15:30:23 +0200

    integrated uvicorn and fastapi

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from w9-pythondropbox!")
+from fastapi import FastAPI
+import uvicorn
 
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+def main():
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload= True)
 
 if __name__ == "__main__":
     main()
