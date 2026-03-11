75c5bbc01c0329c4946f192d94d9320bc21945b4
GabrielaSzuiogan <Gabriela.Szuiogan@cognizant.com>
2026-03-11 14:34:06 +0200

    upd imports

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
-from api.routes import core_router
+from api.routes import core_router, auth_router
 
 app = FastAPI()
 
 app.include_router(core_router)
+app.include_router(auth_router)
 
 def main():
     uvicorn.run("main:app", port=8000, host="0.0.0.0", reload= True)
